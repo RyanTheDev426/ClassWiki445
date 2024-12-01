f53f2eac4e742abbac6b121b829af5c4465473c2
RyanTheDev426 <orth.ryane@gmail.com>
2024-12-01 12:26:46 -0700

    fix errors in TryitPageRating

--- TryitPageRating/Default.aspx.cs
+++ TryitPageRating/Default.aspx.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.ServiceModel.Security;
 using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Xml.Linq;
 
 namespace TryitPageRating
 {
