968e4fbbe9766f8e3a3e7c05f9e541df0530e74e
Chaitra Daggubati <daggubatic@gmail.com>
2024-12-01 17:48:09 -0800

    course list

--- StaffPage/Default.aspx.cs
+++ StaffPage/Default.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
 
 namespace StaffPage
 {
@@ -11,7 +12,34 @@ namespace StaffPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {              
+                XmlDocument xmlDoc = new XmlDocument();
+                string filePath = Server.MapPath("~/App_Data/Classes.xml");
+                xmlDoc.Load(filePath);              
+                XmlNode rootNode = xmlDoc.DocumentElement;             
+                List<Class> classNames = new List<Class>();            
+                foreach (XmlNode classNode in rootNode.ChildNodes) {
+                  XmlNode nameNode = classNode.SelectSingleNode("Name");
+                   XmlNode professorNode = classNode.SelectSingleNode("Professor");
+                    classNames.Add(new Class { Name = nameNode.InnerText, Professor = professorNode.InnerText}); } 
+                ListView1.DataSource = classNames; ListView1.DataBind(); }
+            }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DataList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
         }
     }
+    public class Class { public string Name { get; set; } public string Professor { get; set; } }
 }
