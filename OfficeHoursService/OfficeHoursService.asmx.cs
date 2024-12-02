6f65d026a573eb2cd79cfa2a84bc67bb22c6e312
Chaitra Daggubati <daggubatic@gmail.com>
2024-12-01 21:39:50 -0800

    class details

--- OfficeHoursService/OfficeHoursService.asmx.cs
+++ OfficeHoursService/OfficeHoursService.asmx.cs
@@ -20,31 +20,32 @@ namespace taOfficeHours
     // [System.Web.Script.Services.ScriptService]
     public class OfficeHoursService : System.Web.Services.WebService
     {
-        
+
         [WebMethod]
         public String AddOfficeHours(String profName, String profTime, String profLocation, String taName, String taTime, String taLocation)
         {
-            XmlDocument doc = new XmlDocument(); 
-            string filePath = Server.MapPath("~/App_Data/officeHours.xml");         
+            XmlDocument doc = new XmlDocument();
+            string filePath = Server.MapPath("~/App_Data/officeHours.xml");
             // Check if the file exists
-            if (!File.Exists(filePath))         {            
-            //Create a new XML document with the root element
-                XmlElement root1 = doc.CreateElement("OfficeHours");             
-                doc.AppendChild(root1);            
-                doc.Save(filePath);        
-            }         
+            if (!File.Exists(filePath))
+            {
+                //Create a new XML document with the root element
+                XmlElement root1 = doc.CreateElement("OfficeHours");
+                doc.AppendChild(root1);
+                doc.Save(filePath);
+            }
             // Load the existing or newly created XML document
-            doc.Load(filePath);         
+            doc.Load(filePath);
             // Get the root element
-            XmlElement root = doc.DocumentElement; 
+            XmlElement root = doc.DocumentElement;
             //Create a new user element
             XmlElement user = doc.CreateElement("OfficeHour");
             root.AppendChild(user); // Add name and age elements to the user element
             XmlElement pNameElement = doc.CreateElement("ProfessorName");
-            pNameElement.InnerText = profName; 
-            user.AppendChild(pNameElement); 
+            pNameElement.InnerText = profName;
+            user.AppendChild(pNameElement);
             XmlElement pTimeElement = doc.CreateElement("ProfessorTime");
-            pTimeElement.InnerText = profTime; 
+            pTimeElement.InnerText = profTime;
             user.AppendChild(pTimeElement); // Save the modified XML document
             XmlElement pLocaElement = doc.CreateElement("ProfessorLocation");
             pLocaElement.InnerText = profLocation;
@@ -62,5 +63,15 @@ namespace taOfficeHours
             return "Office Hours Added.";
         }
 
+        [WebMethod]
+        public String GetOfficeHours(String profName)
+        {
+            XmlDocument doc = new XmlDocument();
+            string filePath = Server.MapPath("~/App_Data/officeHours.xml");
+            doc.Load(filePath);
+            XmlNode rootNode = doc.DocumentElement;
+            XmlNode targetClassNode = rootNode.SelectSingleNode("OfficeHour[ProfessorName='" + profName + "']");
+            return targetClassNode.OuterXml;
+        }
     }
 }
